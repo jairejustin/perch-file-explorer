8d568e1134e733605cb4335687ac6c569b13f15d
jairejustin <justinjairebanzuela.bunos25@bicol-u.edu.ph>
2026-02-25 09:55:55 +0800

    small ui changes

--- src/components/file-list/FileList.tsx
+++ src/components/file-list/FileList.tsx
@@ -2,13 +2,19 @@ import { useEffect } from 'react';
 import { invoke } from '@tauri-apps/api/core';
 import * as tauriPath from '@tauri-apps/api/path';
 import { useExplorerStore, type FileEntry } from '../../store/explorerStore';
+import { HugeiconsIcon } from '@hugeicons/react';
+import { ICON_SIZES, STROKE_SIZES } from '../../lib/constants';
+import { 
+  File01Icon,
+  Folder01Icon
+} from '@hugeicons/core-free-icons';
 import './FileList.css';
 
 function FileList() {
-  const { currentPath, files, setFiles, setCurrentPath } = useExplorerStore();
+  const { currentPath, files, setFiles, navigate } = useExplorerStore();
 
   useEffect(() => {
-    tauriPath.homeDir().then(setCurrentPath).catch(console.error);
+    tauriPath.homeDir().then(navigate).catch(console.error);
   }, []);
 
   useEffect(() => {
@@ -27,9 +33,14 @@ function FileList() {
           <li
             key={file.path}
             className={`file-list__item${file.isDir ? ' file-list__item--dir' : ''}`}
-            onClick={() => file.isDir && setCurrentPath(file.path)}
+            onClick={() => file.isDir && navigate(file.path)}
           >
-            <span className="file-list__icon">{file.isDir ? '📁' : '📄'}</span>
+            <span className="file-list__icon">
+              {file.isDir ? 
+                <HugeiconsIcon icon={Folder01Icon} size={ICON_SIZES.m} color="currentColor" strokeWidth={STROKE_SIZES.thin} />
+                : <HugeiconsIcon icon={File01Icon} size={24} color="currentColor" strokeWidth={STROKE_SIZES.thin} />
+              }
+            </span>
             <span className="file-list__name">{file.name}</span>
           </li>
         ))
